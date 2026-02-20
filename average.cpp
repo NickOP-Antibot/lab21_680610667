5ff4d2b96091bec966c0cdc8417dfbdb3eda1138
NickOP-Antibot <chitsanupat2549@gmail.com>
2026-02-20 16:36:58 +0700

    average.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
 
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    float sum;
+    if (argc >1) {
+        for (int i = 1; i < argc; i++) sum += stof(argv[i]);
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1) << endl;
+        cout << "---------------------------------" << endl;
+    } else cout << "Please input numbers to find average." << endl;
+    return 0;
+}
